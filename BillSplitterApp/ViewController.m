abac2b06dce6aadddbc6166ae535145a27f9fbdc
Wiljay <wiljay.flores@gmail.com>
2018-08-12 22:15:41 -0400

    added button action for calculate

--- BillSplitterApp/ViewController.m
+++ BillSplitterApp/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billAmount;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UILabel *splitAmount;
 
 @end
 
@@ -23,6 +26,19 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)calculateSplitAmount:(UIButton *)sender {
+    
+    NSDecimalNumber *people = [[NSDecimalNumber alloc] initWithFloat:self.slider.value];
+    NSDecimalNumber *bill = [[NSDecimalNumber alloc] initWithString:self.billAmount.text];
+    NSDecimalNumberHandler *behaviour = [NSDecimalNumberHandler decimalNumberHandlerWithRoundingMode:NSRoundPlain scale:0 raiseOnExactness:NO raiseOnOverflow:NO raiseOnUnderflow:NO raiseOnDivideByZero:NO];
+    NSDecimalNumberHandler *twoDecimal = [NSDecimalNumberHandler decimalNumberHandlerWithRoundingMode:NSRoundPlain scale:2 raiseOnExactness:NO raiseOnOverflow:NO raiseOnUnderflow:NO raiseOnDivideByZero:NO];
+   
+    
+    people = [people decimalNumberByRoundingAccordingToBehavior:behaviour];
+    
+    self.splitAmount.text = [NSString stringWithFormat:@"Split Amount: $%@",[[bill decimalNumberByDividingBy:people] decimalNumberByRoundingAccordingToBehavior:twoDecimal]];
+    
 }
 
 
